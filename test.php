e46f0657b0acb447d93351bd8fb7d7822a7d0c0f
ThomasZero <ehlers@uam-group.de>
2017-07-05 11:50:53 +0200

    PHP Code

--- test.php
+++ test.php
@@ -1,1 +1,9 @@
 
+<?php
+    echo 'Dies ist ein Test'; // Dies ist ein einzeiliger Kommentar im C++-Stil
+    /* Dies ist ein mehrzeiliger Kommentar
+       noch eine weitere Kommentar-Zeile */
+    echo 'Dies ist noch ein Test';
+    echo '... und ein letzter Test'; # Dies ist ein einzeiliger Shell-Kommentar
+?>
+
